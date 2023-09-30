ddd8521a80ec7ccdd1e2bc58b77bb8eaf0fe0195
aditya15062 <109385859+aditya15062@users.noreply.github.com>
2023-09-30 23:16:28 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+console.log(null>0); // false
+console.log(null==0); //false
+console.log(null>=0);  //true
+
+console.log("2"==2); //true as conversion takes place
+
+//===
+console.log("2"===2); //false as datatype is also checked
