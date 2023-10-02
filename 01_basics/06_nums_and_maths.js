b1a2b16d4152c82531b6acbdccdf61cb3e485abb
aditya15062 <109385859+aditya15062@users.noreply.github.com>
2023-10-02 23:52:38 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const balance= new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2)); //upto 2 decimal places now i.e 100.00
+
+/////////////////////MATHS//////////////
+
+console.log(Math.abs(-3));
+console.log(Math.min(2,7,1,7,9));
+
+console.log(Math.random()) //value lies between 0 and 1;
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
