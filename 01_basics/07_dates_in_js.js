c1b4dab6d9f69a974b9816dbf1153fb3cd58c64d
aditya15062 <109385859+aditya15062@users.noreply.github.com>
2023-10-04 02:07:31 +0000

    dates in js

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,11 @@
+//Dates
+
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(typeof mydate); //object
+
+let mytimestamp=Data.now()
+console.log(mytimestamp.getTime());
+
+mydate.toLocaleString('default',{weekday: "long"})
